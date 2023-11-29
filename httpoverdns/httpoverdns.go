c0527bcd1f4b31327e9eb0df40ee684646271dc3
Bhasher <github.it@bhasher.com>
2023-11-29 08:51:03 +0100

    First version of chunks

--- httpoverdns/httpoverdns.go
+++ httpoverdns/httpoverdns.go
@@ -26,43 +26,63 @@ func (e HTTPOverDNS) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.
 
 	if state.QType() == dns.TypeTXT && strings.HasSuffix(domain, ".l.") {
 
-		fmt.Println(domain)
+		parts := strings.Split(strings.TrimSuffix(domain, ".l."), ".")
 
-		bdecoded, err := base64.Encoding.Strict(*base64.StdEncoding).DecodeString(strings.ReplaceAll(strings.TrimSuffix(domain, ".l."), "_", "="))
+		uid := parts[0]
+		isLastChunk := parts[1] == "1"
+		chunk := parts[2]
+
+		fmt.Println(parts)
+
+		chunks, exists := ctx.Value(uid).(string)
+		if !exists {
+			chunks = ""
+		}
+
+		chunks += chunk
+
+		fmt.Println(chunks)
+
+		ctx = context.WithValue(ctx, uid, chunks)
 
 		text := []byte("")
 
-		if err != nil {
-			text = []byte("1." + err.Error())
+		if !isLastChunk {
+			text = []byte("OK")
 		} else {
-			// decoded := strings.TrimSpace(string(bdecoded))
-			decoded := string(bdecoded)
-
-			fmt.Println("decoded: " + decoded)
-
-			req, err := http.ReadRequest(bufio.NewReader(strings.NewReader(decoded)))
+			bdecoded, err := base64.Encoding.Strict(*base64.StdEncoding).DecodeString(strings.ReplaceAll(strings.TrimSuffix(chunks, ".l."), "_", "="))
 
 			if err != nil {
-				text = []byte("2." + err.Error())
+				text = []byte("1." + err.Error())
 			} else {
+				// decoded := strings.TrimSpace(string(bdecoded))
+				decoded := string(bdecoded)
 
-				client := &http.Client{}
-
-				resp, err := client.Do(req)
-
-				fmt.Println(resp.StatusCode)
+				req, err := http.ReadRequest(bufio.NewReader(strings.NewReader(decoded)))
 
 				if err != nil {
-					text = []byte("3." + err.Error())
+					text = []byte("2." + err.Error())
 				} else {
-					defer resp.Body.Close()
 
-					body, err := io.ReadAll(resp.Body)
+					client := &http.Client{}
+
+					resp, err := client.Do(req)
+
+					fmt.Println(resp.StatusCode)
 
 					if err != nil {
-						text = []byte("4." + err.Error())
+						text = []byte("3." + err.Error())
 					} else {
-						text = body
+						defer resp.Body.Close()
+
+						body, err := io.ReadAll(resp.Body)
+
+						if err != nil {
+							text = []byte("4." + err.Error())
+						} else {
+							text = body
+							fmt.Println("Response")
+						}
 					}
 				}
 			}
@@ -73,10 +93,10 @@ func (e HTTPOverDNS) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.
 
 		header := dns.RR_Header{Name: state.QName(), Rrtype: dns.TypeTXT, Class: dns.ClassINET, Ttl: 0}
 
-		chunks := splitText(strings.ReplaceAll(base64.StdEncoding.EncodeToString(text), "=", "_"), 255)
+		rchunks := splitText(strings.ReplaceAll(base64.StdEncoding.EncodeToString(text), "=", "_"), 255)
 
-		for _, chunk := range chunks {
-			txtRecord := &dns.TXT{Hdr: header, Txt: []string{chunk}}
+		for _, rchunk := range rchunks {
+			txtRecord := &dns.TXT{Hdr: header, Txt: []string{rchunk}}
 			msg.Answer = append(msg.Answer, txtRecord)
 		}
 
--- local-proxy/proxy-http.py
+++ local-proxy/proxy-http.py
@@ -3,11 +3,16 @@ from urllib import request
 import shutil
 import dns.resolver
 import base64
+import random
 
 PORT = 1080
 
 resolver = dns.resolver.Resolver(configure=False)
 resolver.nameservers = ['127.0.0.1']
+
+def split_string_into_chunks(input_string, chunk_size):
+    return [input_string[i:i + chunk_size] for i in range(0, len(input_string), chunk_size)]
+
 
 class HTTPProxy(BaseHTTPRequestHandler):
     def get_raw_request(self):
@@ -38,27 +43,31 @@ class HTTPProxy(BaseHTTPRequestHandler):
 
     def handle_request(self):
         print("RAW:", self.get_raw_request())
-        domain = base64.b64encode(self.get_raw_request()).decode('utf-8').replace('=', '_') + '.l'
-        print(domain)
-        try:
-            r = resolver.resolve(domain, 'TXT')
+        content = base64.b64encode(self.get_raw_request()).decode('utf-8').replace('=', '_')
 
+        rid = random.randint(0, 99999)
+
+        chunks = split_string_into_chunks(content, 53)
+
+        try:
+            for i, chunk in enumerate(chunks):
+                domain = f'{rid}.{int(i == len(chunks)-1)}.{chunk}.l' 
+                r = resolver.resolve(domain, 'TXT')
+            
             rep = ''
 
             for i in r.response.answer:
                 for j in i.items:
                     rep += j.to_text().replace('_', '=')
 
-            print(rep)
-
             response = base64.b64decode(rep)
 
             self.send_response(200)
             self.end_headers()
             self.wfile.write(response)
+
         except Exception as e:
             self.send_error(500, str(e))
-
 
 httpd = HTTPServer(('', PORT), HTTPProxy)
 print("Now serving at", PORT)
